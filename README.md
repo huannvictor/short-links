e3f5dd8618a47d60c1bcbecbfa70d18c426dec87
Huann Almeida <huannvictor@gmail.com>
2024-03-21 14:02:34 -0300

    feat: add routes

---
+++ README.md
@@ -0,0 +1,14 @@
+```bash
+npm install
+```
+to install all dependencies
+
+```bash
+docker compose up -d
+```
+to upload the docker images of postgres and redis
+
+```bash
+npm run setup
+```
+to create the postgres database
--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,46 @@
 import fastify from 'fastify'
 import { z } from "zod"
 import { sql } from "./lib/postgres"
+import postgres from "postgres"
+import { redis } from "./lib/redis"
 
 const app = fastify()
 
-app.post('/links', async (request) => {
+app.get('/:code', async (request, reply) => {
+  const getLinksSchema = z.object({
+    code: z.string().min(3)
+  })
+
+  const { code } = getLinksSchema.parse(request.params)
+
+  const result = await sql/*sql*/`
+    SELECT id, original_url
+    FROM short_links
+    WHERE short_links.code = ${code}
+  `
+
+  if (result.length === 0) {
+    return reply.status(400).send({ message: 'Link not found.' })
+  }
+
+  const link = result[0]
+
+  await redis.zIncrBy('metrics', 1, link.id)
+
+  return reply.redirect(301, link.original_url)
+})
+
+app.get('/api/links', async () => {
+  const result = await sql/*sql*/`
+    SELECT *
+    FROM short_links
+    ORDER BY created_at DESC
+  `
+
+  return result
+})
+
+app.post('/api/links', async (request, reply) => {
   const createLinkSchema = z.object({
     code: z.string().min(3),
     url: z.string().url()
@@ -12,11 +48,28 @@ app.post('/links', async (request) => {
 
   const { code, url } = createLinkSchema.parse(request.body)
 
-  const result = await sql/*sql*/`
-    INSERT INTO short_links (code, original_url)
-    VALUES (${code}) (${url})
-    RETURNING id
-  `
+  try {
+    const result = await sql/*sql*/`
+      INSERT INTO short_links (code, original_url)
+      VALUES (${code}, ${url})
+      RETURNING id
+    `
+
+    const link = result[0]
+
+    return reply.status(201).send({ shortLinkId: link.id })
+
+  } catch (error) {
+    if (error instanceof postgres.PostgresError) {
+      if (error.code === '23505') {
+        return reply.status(400).send({ message: 'Duplicated code!' })
+      }
+    }
+
+    console.error(error)
+
+    return reply.status(500).send({ message: 'Internal error' })
+  }
 })
 
 app.listen({ port: 3333 }).then(() => {
